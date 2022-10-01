4fcf7bb03adcf536017f17f3cd6a42c4d1495fdc
Robert Arnold <robertarnold391@gmail.com>
2022-10-01 10:09:35 +0100

    Messing around with animation

---
+++ LD51/Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+
+    void Start()
+    {
+        Physics2D.IgnoreLayerCollision(8, 8);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ LD51/Assets/Scripts/LimbTracker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimbTracker : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_target;
+    [SerializeField]
+    private GameObject m_relative;
+    [SerializeField]
+    private float m_force = 10.0f;
+
+    private Rigidbody2D m_rigidbody = null;
+
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = m_target.transform.position - m_relative.transform.position;
+        direction.Normalize();
+        m_rigidbody.AddForceAtPosition(direction * m_force * Time.fixedDeltaTime, m_relative.transform.position);
+    }
+}
